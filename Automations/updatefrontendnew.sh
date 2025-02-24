24a63d9948c9b34c8ef5023a2639135fd4c6ae65
emizentech38 <emizentech38@gmail.com>
2025-02-24 18:12:25 +0530

    Update updatefrontendnew.sh

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-# Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
-
-# Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Retrieve the host machine's local IP address
+ipv4_address=$(hostname -I | awk '{print $1}')
 
 # Path to the .env file
 file_to_find="../frontend/.env.docker"
 
-# Check the current VITE_API_PATH in the .env file
+# Check if the file exists
+if [ ! -f "$file_to_find" ]; then
+    echo "ERROR: File not found."
+    exit 1
+fi
+
+# Check the current VITE_API_PATH value
 current_url=$(cat $file_to_find)
 
 # Update the .env file if the IP address has changed
 if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:31100\"" ]]; then
-    if [ -f $file_to_find ]; then
-        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:31100\"|g" $file_to_find
-    else
-        echo "ERROR: File not found."
-    fi
+    sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:31100\"|g" "$file_to_find"
+    echo "Updated VITE_API_PATH to http://${ipv4_address}:31100"
+else
+    echo "VITE_API_PATH is already up to date."
 fi
